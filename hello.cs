758415eb8cf806b91a367c023a5ce6b2e4b6b759
Jeremy Cole <jeremy@jcole.us>
2017-05-06 12:08:46 -0700

    Spaces in C#

--- hello.cs
+++ hello.cs
@@ -1,6 +1,6 @@
 class Hello {
     static void Main(string[] args) {
-        for(int i=0; i<10; i++) {
+        for(int i = 0; i < 10; i++) {
             System.Console.WriteLine("Hello, World!");
         }
     }
