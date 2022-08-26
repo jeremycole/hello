6acb5a203208d874daaca2d0ab639862cd87ac15
Liam Cole <liam@liamiam.com>
2022-08-25 17:55:33 -0700

    Visual Basic

---
+++ hello.vb
@@ -0,0 +1,12 @@
+' Visual Basic
+
+' This is Hello World
+' written in Visual Basic.
+
+Module Hello
+    Sub Main(args As String())
+        For i = 0 To 10
+            System.Console.WriteLine("Hello World!") ' Greetings
+        Next
+    End Sub
+End Module
